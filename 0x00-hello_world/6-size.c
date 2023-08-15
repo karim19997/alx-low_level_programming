a6d6d6628209b5aadca05bb6df39cf810eb15b30
karim ashraf <karimashraf19799@gmail.com>
2023-08-14 17:06:04 -0700

    ii

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'the program's description'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char a;
+int b;
+float f;
+long int c;
+long long int d;
+printf("Size of int: %ld bytes\n", sizeof(b));
+printf("Size of float: %ld bytes\n", sizeof(f));
+printf("Size of long int: %ld byte\n", sizeof(c);
+printf("Size of long long int : %ld byte\n", sizeof(d);
+printf("Size of char: %ld byte\n", sizeof(a);
+return (0);
+}
